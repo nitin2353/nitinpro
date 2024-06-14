3294cdfdc35c81f7a5e2ebc5bfbebfd68c996185
nitin2353 <154704828+nitin2353@users.noreply.github.com>
2024-06-14 11:05:39 -0700

    Add files via upload

---
+++ keyboard/script.js
@@ -0,0 +1,382 @@
+document.addEventListener("keydown", function (event) {
+    if (event.keyCode === 27) {
+        document.getElementById("row1-1").style = "background:#196349;"
+    }
+    if (event.key === 'F1') {
+        event.preventDefault();
+        document.getElementById("row1-2").style = "background:#196349;"
+    }
+    if (event.key === 'F2') {
+        event.preventDefault();
+        document.getElementById("row1-3").style = "background:#196349;"
+    }
+    if (event.key === 'F3') {
+        event.preventDefault();
+        document.getElementById("row1-4").style = "background:#196349;"
+    }
+    if (event.key === 'F4') {
+        event.preventDefault();
+        document.getElementById("row1-5").style = "background:#196349;"
+    }
+    if (event.key === 'F5') {
+        event.preventDefault();
+        document.getElementById("row1-6").style = "background:#196349;"
+    }
+    if (event.key === 'F6') {
+        event.preventDefault();
+        document.getElementById("row1-7").style = "background:#196349;"
+    }
+    if (event.key === 'F7') {
+        event.preventDefault();
+        document.getElementById("row1-8").style = "background:#196349;"
+    }
+    if (event.key === 'F8') {
+        event.preventDefault();
+        document.getElementById("row1-9").style = "background:#196349;"
+    }
+    if (event.key === 'F9') {
+        event.preventDefault();
+        document.getElementById("row1-10").style = "background:#196349;"
+    }
+    if (event.key === 'F10') {
+        event.preventDefault();
+        document.getElementById("row1-11").style = "background:#196349;"
+    }
+    if (event.key === 'F11') {
+        event.preventDefault();
+        document.getElementById("row1-12").style = "background:#196349;"
+    }
+    if (event.key === 'F12') {
+        event.preventDefault();
+        document.getElementById("row1-13").style = "background:#196349;"
+    }
+    if (event.key == '`') {
+        document.getElementById("row2-1").style = "background:#196349;"
+    }
+    if (event.keyCode == 49) {
+        document.getElementById("row2-2").style = "background:#196349;"
+    }
+    if (event.keyCode == 50) {
+        document.getElementById("row2-3").style = "background:#196349;"
+    }
+    if (event.keyCode == 51) {
+        document.getElementById("row2-4").style = "background:#196349;"
+    }
+    if (event.keyCode == 52) {
+        document.getElementById("row2-5").style = "background:#196349;"
+    }
+    if (event.keyCode == 53) {
+        document.getElementById("row2-6").style = "background:#196349;"
+    }
+    if (event.keyCode == 54) {
+        document.getElementById("row2-7").style = "background:#196349;"
+    }
+    if (event.keyCode == 55) {
+        document.getElementById("row2-8").style = "background:#196349;"
+    }
+    if (event.keyCode == 56) {
+        document.getElementById("row2-9").style = "background:#196349;"
+    }
+    if (event.keyCode == 57) {
+        document.getElementById("row2-10").style = "background:#196349;"
+    }
+    if (event.keyCode == 48) {
+        document.getElementById("row2-11").style = "background:#196349;"
+    }
+    if (event.key == '-') {
+        document.getElementById("row2-12").style = "background:#196349;"
+    }
+    if (event.key == '=') {
+        document.getElementById("row2-13").style = "background:#196349;"
+    }
+    if (event.keyCode == 08) {
+        document.getElementById("row2-14").style = "background:#196349;"
+    }
+
+    // row 3
+
+    if (event.keyCode == 9) {
+        event.preventDefault();
+        document.getElementById("row3-1").style = "background:#196349;"
+    }
+    if (event.key == 'q') {
+        document.getElementById("row3-2").style = "background:#196349;"
+    }
+    if (event.key == 'w') {
+        document.getElementById("row3-3").style = "background:#196349;"
+    }
+    if (event.key == 'e') {
+        document.getElementById("row3-4").style = "background:#196349;"
+    }
+    if (event.key == 'r') {
+        document.getElementById("row3-5").style = "background:#196349;"
+    }
+    if (event.key == 't') {
+        document.getElementById("row3-6").style = "background:#196349;"
+    }
+    if (event.key == 'y') {
+        document.getElementById("row3-7").style = "background:#196349;"
+    }
+    if (event.key == 'u') {
+        document.getElementById("row3-8").style = "background:#196349;"
+    }
+    if (event.key == 'i') {
+        document.getElementById("row3-9").style = "background:#196349;"
+    }
+    if (event.key == 'o') {
+        document.getElementById("row3-10").style = "background:#196349;"
+    }
+    if (event.key == 'p') {
+        document.getElementById("row3-11").style = "background:#196349;"
+    }
+    if (event.key == '[') {
+        document.getElementById("row3-12").style = "background:#196349;"
+    }
+    if (event.key == ']') {
+        document.getElementById("row3-13").style = "background:#196349;"
+    }
+    if (event.key == '\\') {
+        document.getElementById("row3-14").style = "background:#196349;"
+    }
+
+    // row 4
+
+    if (event.keyCode == '20') {
+        event.preventDefault();
+        document.getElementById("row4-1").style = "background:#196349;"
+    }
+    if (event.key == 'a') {
+        document.getElementById("row4-2").style = "background:#196349;"
+    }
+    if (event.key == 's') {
+        document.getElementById("row4-3").style = "background:#196349;"
+    }
+    if (event.key == 'd') {
+        document.getElementById("row4-4").style = "background:#196349;"
+    }
+    if (event.key == 'f') {
+        document.getElementById("row4-5").style = "background:#196349;"
+    }
+    if (event.key == 'g') {
+        document.getElementById("row4-6").style = "background:#196349;"
+    }
+    if (event.key == 'h') {
+        document.getElementById("row4-7").style = "background:#196349;"
+    }
+    if (event.key == 'j') {
+        document.getElementById("row4-8").style = "background:#196349;"
+    }
+    if (event.key == 'k') {
+        document.getElementById("row4-9").style = "background:#196349;"
+    }
+    if (event.key == 'l') {
+        document.getElementById("row4-10").style = "background:#196349;"
+    }
+    if (event.key == ";") {
+        document.getElementById("row4-11").style = "background:#196349;"
+    }
+    if (event.key == "'") {
+        document.getElementById("row4-12").style = "background:#196349;"
+    }
+    if (event.key == 'Enter') {
+        document.getElementById("row4-13").style = "background:#196349;"
+    }
+    if (event.key == '') {
+        document.getElementById("row4-14").style = "background:#196349;"
+    }
+
+    // row 5
+
+    if (event.key == 'Shift2') {
+        event.preventDefault()
+        document.getElementById("row5-1").style = "background:#196349;"
+    }
+    if (event.key == 'z') {
+        document.getElementById("row5-2").style = "background:#196349;"
+    }
+    if (event.key == 'x') {
+        document.getElementById("row5-3").style = "background:#196349;"
+    }
+    if (event.key == 'c') {
+        document.getElementById("row5-4").style = "background:#196349;"
+    }
+    if (event.key == 'v') {
+        document.getElementById("row5-5").style = "background:#196349;"
+    }
+    if (event.key == 'b') {
+        document.getElementById("row5-6").style = "background:#196349;"
+    }
+    if (event.key == 'n') {
+        document.getElementById("row5-7").style = "background:#196349;"
+    }
+    if (event.key == 'm') {
+        document.getElementById("row5-8").style = "background:#196349;"
+    }
+    if (event.key == ',') {
+        document.getElementById("row5-9").style = "background:#196349;"
+    }
+    if (event.key == '.') {
+        document.getElementById("row5-10").style = "background:#196349;"
+    }
+    if (event.key == 'Shift') {
+        event.preventDefault();
+        document.getElementById("row5-11").style = "background:#196349;"
+    }
+
+    // row 6
+
+    if (event.ctrlKey) {
+        event.preventDefault()
+        document.getElementById("row6-1").style = "background:#196349;"
+    }
+    if (event.key == "Windows") {
+        event.preventDefault();
+        document.getElementById("row6-2").style = "background:#196349;"
+    }
+    if (event.key == 'Alt') {
+        event.preventDefault();
+        document.getElementById("row6-3").style = "background:#196349;"
+    }
+    if (event.keyCode == 32) {
+        document.getElementById("row6-4").style = "background:#196349;"
+    }
+
+    if (event.ctrlKey) {
+        event.preventDefault()
+        document.getElementById("row6-7").style = "background:#196349;"
+    }
+    if (event.key == "Windows") {
+        event.preventDefault();
+        document.getElementById("row6-2").style = "background:#196349;"
+    }
+    if (event.key == 'Alt') {
+        event.preventDefault();
+        document.getElementById("row6-5").style = "background:#196349;"
+    }
+
+
+    // opposite
+
+    if (event.key == 'Insert') {
+        event.preventDefault();
+        document.getElementById("row2-15").style = "background:#196349;"
+    }
+    if (event.key == 'Home') {
+        event.preventDefault();
+        document.getElementById("row2-16").style = "background:#196349;"
+    }
+    if (event.key == 'PageUp') {
+        event.preventDefault();
+        document.getElementById("row2-17").style = "background:#196349;"
+    }
+
+    if (event.key == 'Delete') {
+        event.preventDefault();
+        document.getElementById("row3-15").style = "background:#196349;"
+    }
+    if (event.key == 'End') {
+        event.preventDefault();
+        document.getElementById("row3-16").style = "background:#196349;"
+    }
+    if (event.key == 'PageDown') {
+        event.preventDefault();
+        document.getElementById("row3-17").style = "background:#196349;"
+    }
+
+    // Arrow keys
+
+    if (event.key == 'ArrowUp') {
+        event.preventDefault();
+        document.getElementById("row5-12").style = "background:#196349;"
+    }
+    if (event.key == 'ArrowDown') {
+        event.preventDefault();
+        document.getElementById("row6-9").style = "background:#196349;"
+    }
+    if (event.key == 'ArrowLeft') {
+        event.preventDefault();
+        document.getElementById("row6-8").style = "background:#196349;"
+    }
+    if (event.key == 'ArrowRight') {
+        event.preventDefault();
+        document.getElementById("row6-10").style = "background:#196349;"
+    }
+
+    // small keyboard
+
+    if (event.key == 'NumLock') {
+        event.preventDefault();
+        document.getElementById("row2-18").style = "background:#196349;"
+    }
+    if (event.key == '/') {
+        event.preventDefault();
+        document.getElementById("row2-19").style = "background:#196349;"
+    }
+    if (event.key == '*') {
+        event.preventDefault();
+        document.getElementById("row2-20").style = "background:#196349;"
+    }
+    if (event.key == '-') {
+        event.preventDefault();
+        document.getElementById("row2-21").style = "background:#196349;"
+    }
+
+    if (event.key == '7') {
+        event.preventDefault();
+        document.getElementById("row3-18").style = "background:#196349;"
+    }
+    if (event.key == '8') {
+        event.preventDefault();
+        document.getElementById("row3-19").style = "background:#196349;"
+    }
+    if (event.key == '9') {
+        event.preventDefault();
+        document.getElementById("row3-20").style = "background:#196349;"
+    }
+
+    if (event.key == '4') {
+        event.preventDefault();
+        document.getElementById("row4-14").style = "background:#196349;"
+    }
+    if (event.key == '5') {
+        event.preventDefault();
+        document.getElementById("row4-15").style = "background:#196349;"
+    }
+    if (event.key == '6') {
+        event.preventDefault();
+        document.getElementById("row4-16").style = "background:#196349;"
+    }
+
+    if (event.key == '1') {
+        event.preventDefault();
+        document.getElementById("row5-13").style = "background:#196349;"
+    }
+    if (event.key == '2') {
+        event.preventDefault();
+        document.getElementById("row5-14").style = "background:#196349;"
+    }
+    if (event.key == '3') {
+        event.preventDefault();
+        document.getElementById("row5-15").style = "background:#196349;"
+    }
+
+
+    if (event.key == 'Enter') {
+        event.preventDefault();
+        document.getElementById("row5-16").style = "background:#196349;"
+    }
+    if (event.key == '.') {
+        event.preventDefault();
+        document.getElementById("row6-12").style = "background:#196349;"
+    }
+    if (event.key == '0') {
+        event.preventDefault();
+        document.getElementById("row6-11").style = "background:#196349;"
+    }
+    if (event.key == '+') {
+        event.preventDefault();
+        document.getElementById("row3-21").style = "background:#196349;"
+    }
+
+
+});
